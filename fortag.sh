5bea901194c1dc55939d81931507f2b3885a7268
Miro Hrončok <miro@hroncok.cz>
2014-07-29 19:39:56 +0200

    Update the fortag script to sort the tags by date

--- fortag.sh
+++ fortag.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 lasttag=''
 total=''
-for tag in `git tag`; do
+for tag in `git for-each-ref --sort=committerdate --format='%(refname)' refs/tags`; do
   if [[ x$lasttag != x ]]; then
-    e="${tag#v}: "
+    e="${tag#refs/tags/v}: "
     e="$e`git  --no-pager log -1 --format=%ai $tag`\n\n"
     e="$e`git --no-pager log --pretty=\"format: * %s\" $lasttag..$tag`\n\n"
     total="$e$total"

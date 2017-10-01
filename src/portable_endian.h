8c8d0dfcb2ab0e5263f1de0bed2de408d59a9468
Chow Loong Jin <hyperair@debian.org>
2017-10-01 19:34:27 +0800

    Change comment style of portable_endian.h to /* */
    Fixes this warning:
    src/portable_endian.h:1:1: error: C++ style comments are not allowed in ISO C90 [-Werror]

--- src/portable_endian.h
+++ src/portable_endian.h
@@ -1,12 +1,13 @@
-// portable_endian.h
-// from https://gist.github.com/panzi/6856583
-// "License": Public Domain
-// I, Mathias Panzenböck, place this file hereby into the public domain. Use it
-// at your own risk for whatever you like.  In case there are jurisdictions
-// that don't support putting things in the public domain you can also consider
-// it to be "dual licensed" under the BSD, MIT and Apache licenses, if you want
-// to. This code is trivial anyway. Consider it an example on how to get the
-// endian conversion functions on different platforms.
+/* portable_endian.h
+ * from https://gist.github.com/panzi/6856583
+ * "License": Public Domain
+ * I, Mathias Panzenböck, place this file hereby into the public domain. Use it
+ * at your own risk for whatever you like.  In case there are jurisdictions
+ * that don't support putting things in the public domain you can also consider
+ * it to be "dual licensed" under the BSD, MIT and Apache licenses, if you want
+ * to. This code is trivial anyway. Consider it an example on how to get the
+ * endian conversion functions on different platforms.
+ */
 
 #ifndef PORTABLE_ENDIAN_H__
 #define PORTABLE_ENDIAN_H__
@@ -64,7 +65,7 @@
 
 #elif defined(__WINDOWS__)
 
-//#	include <winsock2.h>
+/* #	include <winsock2.h> */
 #	include <sys/param.h>
 
 #	if BYTE_ORDER == LITTLE_ENDIAN

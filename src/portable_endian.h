938dbfe705afd9fb44017657dc30388ca47f80ed
Fernando <fernando.apesteguia@gmail.com>
2019-10-30 20:55:24 +0100

    Fix FreeBSD build
    FreeBSD already provides be16toh, le16toh, etc, so there's no need for that
    definitions.
    
    See https://svnweb.freebsd.org/base/head/sys/sys/endian.h?view=markup
    
    This builds in FreeBSD {11.1,11.2,11.3}{i386,amd64} and 13-CURRENT amd64

--- src/portable_endian.h
+++ src/portable_endian.h
@@ -54,6 +54,7 @@
 
 #	include <sys/endian.h>
 
+#if !defined(__FreeBSD__)
 #	define be16toh(x) betoh16(x)
 #	define le16toh(x) letoh16(x)
 
@@ -62,6 +63,7 @@
 
 #	define be64toh(x) betoh64(x)
 #	define le64toh(x) letoh64(x)
+#endif /*!defined(__FreeBSD__)*/
 
 #elif defined(__WINDOWS__)
 
